ed71f2463bff3ebf674e4f64f1437e34d4455a04
Ryan.Amps <Ryan.Amps@AGLPC-10992>
2024-01-31 14:28:21 +0000

    Initial Commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName    = '8x8Work'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64          = 'https://vod-updates.8x8.com/ga/work-64-msi-v8.9.3-1.msi'
+$checksum64     = '64FEA48CE93A05A628B5C62016922469AA88452223435D4593E53F382B38418C'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'MSI'
+  url64          = $url64
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/quiet /norestart'
+  softwareName   = '8x8 - Work'
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs  
