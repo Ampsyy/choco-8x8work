0039ca33defd9b6194cb9604ec6ebc86f2bcbd44
Ryan Amps <Ryan.Amps@activate-group.com>
2024-03-07 08:46:04 +0000

    Updated URL

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 $packageName    = '8x8Work'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64          = 'https://vod-updates.8x8.com/ga/work-64-msi-v8.10.3-2.msi'
+$url64          = 'https://vod-updates.8x8.com/ga/work-64-msi-v8.10.4-2.msi'
 $checksum64     = 'B0753C8DAA610B27047068C0740B1E1CD837CBC2703957E44618D1AEC65A4852'
 
 $packageArgs = @{
